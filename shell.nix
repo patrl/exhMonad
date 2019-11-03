b1fabee9b66b3dd34afd14db83f3539112666c85
Patrick Elliott <patrick.d.elliott@gmail.com>
2019-11-03 15:12:35 -0500

    syntax highlighting should work now

--- shell.nix
+++ shell.nix
@@ -1,21 +1,25 @@
 let
   jupyter = import (builtins.fetchGit {
     url = https://github.com/tweag/jupyterWith;
-    rev = "10d64ee254050de69d0dc51c9c39fdadf1398c38";
+    rev = "";
   }) {};
 
-  ihaskell = jupyter.kernels.iHaskellWith {
-    name = "haskell";
-    packages = p: with p; [ hvega formatting HaTeX ihaskell-hatex ];
-  };
+  ihaskellWithPackages = jupyter.kernels.iHaskellWith {
+      name = "hExh";
+      packages = p: with p; [
+        HaTeX
+        ihaskell-hatex
+      ];
+    };
 
-  ipython = jupyter.kernels.iPythonWith {
-    name = "python";
-    packages = p: with p; [ numpy ];
-  };
-
-  jupyterEnvironment = jupyter.jupyterlabWith {
-    kernels = [ ihaskell ipython ];
-  };
+  jupyterlabWithKernels =
+    jupyter.jupyterlabWith {
+      kernels = [ ihaskellWithPackages ];
+      directory = jupyter.mkDirectoryWith {
+        extensions = [
+          "jupyterlab-ihaskell"
+        ];
+      };
+    };
 in
-  jupyterEnvironment.env
+  jupyterlabWithKernels.env
