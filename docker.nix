baf9a63313ef7f7e27c3e3be7087f461c57f070e
Patrick Elliott <patrick.d.elliott@gmail.com>
2019-11-05 17:59:58 -0500

    Added a docker.nix file -- note that this doesn't quite work yet.

---
+++ docker.nix
@@ -0,0 +1,27 @@
+let
+  jupyter = import (builtins.fetchGit {
+    url = https://github.com/tweag/jupyterWith;
+    rev = "";
+  });
+  ihaskellWithPackages = jupyter.kernels.iHaskellWith {
+      name = "hExh";
+      packages = p: with p; [
+        (p.callPackage ./hExh {})
+        HaTeX
+        ihaskell-hatex
+      ];
+    };
+  jupyterlabWithKernels =
+    jupyter.jupyterlabWith {
+      kernels = [ ihaskellWithPackages ];
+      directory = jupyter.mkDirectoryWith {
+        extensions = [
+          "jupyterlab-ihaskell"
+        ];
+      };
+    };
+in
+  jupyter.mkDockerImage {
+    name = "jupyter-image";
+    jupyterlab = jupyterlabWithKernels;
+  }
