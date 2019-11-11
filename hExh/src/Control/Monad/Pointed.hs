1250bfff6067df2e61526b4b3fb93d98839dabb7
Patrick Elliott <patrick.d.elliott@gmail.com>
2019-11-11 14:38:53 -0500

    Added pointed set monad

---
+++ hExh/src/Control/Monad/Pointed.hs
@@ -0,0 +1,21 @@
+module Control.Monad.Pointed where
+
+import Control.Monad (liftM,ap)
+
+data Pointed a = Pointed a [a]
+  deriving (Eq, Ord, Show, Read)
+
+toList :: Pointed a -> [a]
+toList (Pointed x xs) = x:xs
+
+instance Functor Pointed where
+  fmap = liftM
+
+instance Applicative Pointed where
+  pure = return
+  (<*>) = ap
+
+instance Monad Pointed where
+  return x = Pointed x []
+  Pointed x xs >>= k = Pointed y (ys ++ (xs >>= toList.k))
+    where Pointed y ys = k x
