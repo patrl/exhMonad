e6a7d27933dfe9a358eb25f61472050653250a10
Patrick Elliott <patrick.d.elliott@gmail.com>
2021-01-21 20:51:57 -0500

    Included more traditional alternatives algorithm

--- src/Data/Logic/Classical/Alternatives.hs
+++ src/Data/Logic/Classical/Alternatives.hs
@@ -3,12 +3,17 @@ module Data.Logic.Classical.Alternatives where
 import Data.Logic.Classical.Syntax
     ( toExpr, BOp(..), Expr(..), UOp(..), Var )
 
--- this algorithm swaps out binary connectives
 alts :: Expr a -> [Expr a]
-alts (Simple a  ) = [Simple a]
+alts (Simple a) = [Simple a]
 alts (Unary op a) = [ Unary op' b | b <- alts a, op' <- altUOps op]
 alts (Binary And a b) = [ Binary Or alt_a alt_b | alt_a <- a:alts a, alt_b <- b:alts b]
 alts (Binary Or a b) = [ Binary And alt_a alt_b | alt_a <- a:alts a, alt_b <- b:alts b]
+
+-- Gives back closure under substitution of binary operators
+alts2 :: Expr a -> [Expr a]
+alts2 (Simple a  ) = [Simple a]
+alts2 (Unary op a) = [ Unary op' b | b <- alts a, op' <- altUOps op]
+alts2 (Binary op a b) = [ Binary op' alt_a alt_b | alt_a <- alts a, alt_b <- alts b, op' <- altBOps op]
 
 altBOps :: BOp -> [BOp]
 altBOps _ = [And,Or]
@@ -22,3 +27,6 @@ testExpr = Binary Or (toExpr 'p') (Binary Or (toExpr 'q') (toExpr 'r'))
 
 -- >>> alts testExpr
 -- [(p ∧ (q ∨ r)),(p ∧ (q ∧ r)),(p ∧ (q ∧ r)),(p ∧ (q ∧ r)),(p ∧ (q ∧ r)),(p ∧ (q ∨ r)),(p ∧ (q ∧ r)),(p ∧ (q ∧ r)),(p ∧ (q ∧ r)),(p ∧ (q ∧ r))]
+
+-- >>> alts2 testExpr
+-- [(p ∧ (q ∧ r)),(p ∨ (q ∧ r)),(p ∧ (q ∧ r)),(p ∨ (q ∧ r)),(p ∧ (q ∧ r)),(p ∨ (q ∧ r)),(p ∧ (q ∧ r)),(p ∨ (q ∧ r))]
