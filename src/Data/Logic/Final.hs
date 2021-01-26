8e109b807150bad3f08f22f3b3aae68cb7599a00
Patrick Elliott <patrick.d.elliott@gmail.com>
2021-01-26 00:52:26 -0500

    More messing around with tagless final

--- src/Data/Logic/Final.hs
+++ src/Data/Logic/Final.hs
@@ -1,11 +1,16 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE ParallelListComp #-}
+{-# LANGUAGE OverloadedLists #-}
 
 module Data.Logic.Final where
 
 import           Control.Applicative            ( Applicative(liftA2) )
 import           Control.Monad                  ( replicateM )
+import           Data.Biapplicative             ( (<<*>>)
+                                                , biliftA2
+                                                , bipure
+                                                )
 import qualified Data.Map                      as M
 import qualified Data.Set                      as S
 
@@ -15,7 +20,7 @@ import qualified Data.Set                      as S
 
 -- The tagless final encoding of boolean expressions.
 class BoolSYM s where
-  {-# MINIMAL top, neg, (conj|disj|impl) #-}
+  {-# MINIMAL top, neg, (conj|disj) #-}
   top :: s
   bot :: s
   bot = neg top
@@ -23,9 +28,22 @@ class BoolSYM s where
   conj :: s -> s -> s
   conj s t = neg (neg s `disj` neg t)
   disj :: s -> s -> s
-  disj s = impl (neg s)
-  impl :: s -> s -> s
-  impl s t = neg (neg s `disj` neg t)
+  disj s t = neg (neg s `conj` neg t)
+
+-- These look like bifunctors
+instance (BoolSYM a,BoolSYM b) => BoolSYM (a,b) where
+  top = (top, top)
+  bot = (bot, bot)
+  neg p = (neg, neg) <<*>> p
+  conj p q = biliftA2 conj conj p q
+  disj p q = biliftA2 disj disj p q
+
+instance (BoolSYM a,BoolSYM b,BoolSYM c) => BoolSYM (a,b,c) where
+  top = (top, top, top)
+  bot = (bot, bot, bot)
+  neg (s, t, u) = (neg s, neg t, neg u)
+  conj (s, t, u) (s', t', u') = (conj s s', conj t t', conj u u')
+  disj (s, t, u) (s', t', u') = (disj s s', disj t t', disj u u')
 
 -- Newtype wrappers to distinguish between ascii and unicode strings.
 newtype A = A String
@@ -38,7 +56,6 @@ instance BoolSYM A where
   neg (A s) = A $ "~" ++ s
   conj (A s) (A t) = A $ "(" ++ s ++ "&" ++ t ++ ")"
   disj (A s) (A t) = A $ "(" ++ s ++ "|" ++ t ++ ")"
-  impl (A s) (A t) = A $ "(" ++ s ++ "->" ++ t ++ ")"
 
 viewA :: A -> String
 viewA (A s) = s
@@ -50,7 +67,6 @@ instance BoolSYM U where
   neg (U s) = U $ "¬" ++ s
   conj (U s) (U t) = U $ "(" ++ s ++ "∧" ++ t ++ ")"
   disj (U s) (U t) = U $ "(" ++ s ++ "∨" ++ t ++ ")"
-  impl (U s) (U t) = U $ "(" ++ s ++ "→" ++ t ++ ")"
 
 viewU :: U -> String
 viewU (U s) = s
@@ -67,19 +83,19 @@ instance BoolSYM Bool where
 eval :: Bool -> Bool
 eval = id
 
--- Evaluation, but we keep track of intermediate results.
+-- -- Evaluation, but we keep track of intermediate results.
 
-instance BoolSYM a => BoolSYM [(a,Bool)] where
-  top = [(top, top)]
-  bot = [(bot, bot)]
-  neg ps = [ (neg f, neg g) | (f, g) <- ps ] ++ [ (f, g) | (f, g) <- ps ]
-  conj ps qs =
-    [ (conj a b, conj p q) | (a, p) <- ps, (b, q) <- qs ]
-      ++ [ (a, b) | (a, b) <- ps ]
-      ++ [ (c, d) | (c, d) <- qs ]
+-- instance BoolSYM a => BoolSYM [(a,Bool)] where
+--   top = [(top, top)]
+--   bot = [(bot, bot)]
+--   neg ps = [ (neg f, neg g) | (f, g) <- ps ] ++ [ (f, g) | (f, g) <- ps ]
+--   conj ps qs =
+--     [ (conj a b, conj p q) | (a, p) <- ps, (b, q) <- qs ]
+--       ++ [ (a, b) | (a, b) <- ps ]
+--       ++ [ (c, d) | (c, d) <- qs ]
 
-stepEval :: [(A, Bool)] -> [(String, Bool)]
-stepEval ps = [ (s, t) | (A s, t) <- ps ]
+-- stepEval :: [(A, Bool)] -> [(String, Bool)]
+-- stepEval ps = [ (s, t) | (A s, t) <- ps ]
 
 ----------------------------------------------
 -- Modular extension to propositional logic --
@@ -88,13 +104,20 @@ stepEval ps = [ (s, t) | (A s, t) <- ps ]
 -- We treat variables as integers.
 type Var = Int
 
--- An assignment is a function from variables to boolean values.
+-- An assignment is a mapping from variables to boolean values.
 type Assignment = M.Map Var Bool
 
 -- 'at' maps variables to expressions of propositional logic, which compose via the boolean operators.
 -- N.b., we don't need to define the boolean operators again for propositional logic.
 class BoolSYM s => PropSYM s where
   at :: Var -> s
+
+-- Some boilerplate duplicator instances.
+instance (PropSYM a, PropSYM b) => PropSYM (a,b) where
+  at n = (at n, at n)
+
+instance (PropSYM a, PropSYM b,PropSYM c) => PropSYM (a,b,c) where
+  at n = (at n, at n, at n)
 
 -- Printing variables as ascii/unicode.
 instance PropSYM A where
@@ -115,24 +138,16 @@ instance PropSYM (S.Set Var) where
 variables :: S.Set Var -> [Var]
 variables = S.toList
 
-instance BoolSYM [Assignment] where
-  top  = mempty
-  neg  = id
-  conj = (++)
-  disj = (++)
+-- We could compute this compositionally, but there isn't much point.
+universe :: S.Set Var -> [Assignment]
+universe vs =
+  [ M.fromList [ (v, t) | v <- variables vs | t <- ts ]
+  | ts <- replicateM (length vs) [True, False]
+  ]
 
-instance PropSYM [Assignment] where
-  at n =
-    let vars = variables (at n)
-    in  [ M.fromList [ (v, t) | v <- vars | t <- ts ]
-        | ts <- replicateM (length vars) [True, False]
-        ]
-
-universe :: [Assignment] -> [Assignment]
-universe = id
-
+-- Note that we're just composing applicative functors here.
 instance BoolSYM (Assignment -> Maybe Bool) where
-  top = const $ pure top
+  top = (pure . pure) top
   neg p = (fmap . fmap) neg p
   conj p q = (liftA2 . liftA2) conj p q
   disj p q = (liftA2 . liftA2) disj p q
@@ -143,7 +158,109 @@ instance PropSYM (Assignment -> Maybe Bool) where
 evalProp :: (Assignment -> Maybe Bool) -> Assignment -> Maybe Bool
 evalProp = id
 
-tf1 = at 1 `conj` at 2 `conj` at 1
+verifiers :: (S.Set Var, Assignment -> Maybe Bool) -> S.Set Assignment
+verifiers (vs, p) = S.fromList [ g | g <- universe vs, p g == Just True ]
 
--- >>> universe tf1
--- [fromList [(1,True)],fromList [(1,False)],fromList [(2,True)],fromList [(2,False)],fromList [(1,True)],fromList [(1,False)]]
+falsifiers :: (S.Set Var, Assignment -> Maybe Bool) -> S.Set Assignment
+falsifiers (vs, p) = S.fromList [ g | g <- universe vs, p g == Just False ]
+
+entails
+  :: (S.Set Var, Assignment -> Maybe Bool)
+  -> (S.Set Var, Assignment -> Maybe Bool)
+  -> Bool
+entails (vs, p) (us, q) =
+  let vs' = vs `S.union` us
+  in  verifiers (vs', p) `S.isSubsetOf` verifiers (vs', q)
+
+data PropType = Tautology | Contradiction | Contingency deriving (Eq,Show)
+
+propType :: (S.Set Var, Assignment -> Maybe Bool) -> PropType
+propType (vs, p) | universe vs == S.toList (verifiers (vs, p)) = Tautology
+                 | universe vs == S.toList (falsifiers (vs, p)) = Contradiction
+                 | otherwise = Contingency
+
+isTautology :: (S.Set Var, Assignment -> Maybe Bool) -> Bool
+isTautology (vs, p) = propType (vs, p) == Tautology
+
+isContradiction :: (S.Set Var, Assignment -> Maybe Bool) -> Bool
+isContradiction (vs, p) = propType (vs, p) == Contradiction
+
+isContingency :: (S.Set Var, Assignment -> Maybe Bool) -> Bool
+isContingency (vs, p) = propType (vs, p) == Contingency
+
+
+--------------------------------------------
+-- indeterminacy and formal alternatives --
+--------------------------------------------
+
+newtype AltScalar s = AltScalar [s] deriving (Functor,Applicative,Semigroup,Monoid)
+
+instance BoolSYM s => BoolSYM (AltScalar s) where
+  top = pure top
+  bot = pure bot
+  neg p = neg <$> p
+  conj p q = liftA2 disj p q
+  disj p q = liftA2 conj p q
+
+instance PropSYM s => PropSYM (AltScalar s) where
+  at n = pure $ at n
+
+alts :: AltScalar s -> [s]
+alts (AltScalar ss) = ss
+
+viewAlts :: AltScalar A -> [String]
+viewAlts (AltScalar as) = viewA <$> as
+
+-- We can blindly negate structural alternatives fairly easily
+
+exhaust :: PropSYM p => (AltScalar p, p) -> p
+exhaust (AltScalar as, p) = p `conj` conjoinNegs as
+
+conjoinNegs :: BoolSYM s => [s] -> s
+conjoinNegs as = foldr conj top (neg <$> as)
+
+tf1 :: PropSYM s => s
+tf1 = at 1 `disj` at 2
+
+-- >>> viewA tf1
+-- "(1|2)"
+
+-- >>> viewAlts tf1
+-- ["(1&2)"]
+
+-- >>> viewA $ exhaust tf1
+-- "((1|2)&(~(1&2)&T))"
+
+-------------------------------
+-- exhaustification operator --
+-------------------------------
+
+class (PropSYM s) => ExhSYM s where
+  exh :: s -> s
+
+instance ExhSYM A where
+  exh (A p) = A $ "O" ++ p
+
+-- Boilperplate duplicator instances.
+
+instance (ExhSYM a, ExhSYM b) => ExhSYM (a,b) where
+  exh p = bipure exh exh <<*>> p
+
+instance (ExhSYM a, ExhSYM b, ExhSYM c) => ExhSYM (a,b,c) where
+  exh (s, t, u) = (exh s, exh t, exh u)
+
+-- Scalar alternatives foe exhaustified formulas.
+instance ExhSYM s => ExhSYM (AltScalar s) where
+  exh p = exh <$> p
+
+tf2 :: ExhSYM s => s
+tf2 = exh tf1
+
+-- >>> viewExhaust tf2
+-- "((1|2)&(~(1&2)&T))"
+
+-- >>> viewExhaust tf3
+-- "((3|((1|2)&(~(1&2)&T)))&(~(3&O(1&2))&T))"
+
+-- >>> viewExhAlts tf2
+-- ["O(1&2)"]
